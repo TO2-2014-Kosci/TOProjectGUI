ad09a387f378c4691099e4bf33fbf55684832557
Michał Suder <michalsuder@wp.pl>
2014-10-20 19:14:34 +0200

    Change ClientServerPrototype

--- Prototypes/JSON&RabbitMQ/Client/src/rabbitMQ/ClientServerPrototype.java
+++ Prototypes/JSON&RabbitMQ/Client/src/rabbitMQ/ClientServerPrototype.java
@@ -18,12 +18,13 @@ public class ClientServerPrototype {
 	private Channel gameChannel;
 	private QueueingConsumer directConsumer;
 	private QueueingConsumer gameConsumer;
-	private String directChannelName;
+	private String directQueueName;
+	private String gameQueueName;
 	
-	public void connectToServerCreateDirectChannelAndConsume() {
+	public void connectToServerCreateChannelsAndConsume() {
 		connect();
-		createDirectChannel();
-		directConsumer = getConsumerForNormalChannel();
+		createDirectChannelAndConsumeAndQueue();
+		createGameChannelAndConsume();
 		handshake();
 	}
 
@@ -37,35 +38,92 @@ public class ClientServerPrototype {
 		}
 	}
 
-	private void createDirectChannel() {
+	private void createDirectChannelAndConsumeAndQueue() {
 		try {
 			directChannel = connection.createChannel();
-			directChannel.queueDeclare(directChannelName, false, false, false, null);
+			directQueueName = directChannel.queueDeclare().getQueue();
+			directConsumer = new QueueingConsumer(directChannel);
+			directChannel.basicConsume(directQueueName, true, directConsumer);
 		} catch (IOException exception){
 			exception.printStackTrace();
 		}
 	}
 	
-	private QueueingConsumer getConsumerForNormalChannel() {
+	private void createGameChannelAndConsume() {
 		try {
-			QueueingConsumer consumer = new QueueingConsumer(directChannel);
-			directChannel.basicConsume(directChannelName, true, consumer);
-			return consumer;
-		} catch (IOException exception) {
+			gameChannel = connection.createChannel();
+			gameConsumer = new QueueingConsumer(gameChannel);
+			gameChannel.basicConsume(gameQueueName, true, gameConsumer);
+		} catch (IOException exception){
 			exception.printStackTrace();
 		}
-		return new QueueingConsumer(directChannel); //to jest ï¿½le
 	}
 	
 	private void handshake() {
 		try {
 			Channel handShakeChannel = connection.createChannel();
 			handShakeChannel.queueDeclare("handShakeChannel", false, false, false, null);
-			handShakeChannel.basicPublish("", "handShakeChannel", null, new HandshakingMessage(directChannelName).getBytes());
+			handShakeChannel.basicPublish("", "handShakeChannel", null, new HandshakingMessage(directQueueName).getBytes());
 			handShakeChannel.close();
 		} catch (IOException exception){
 			exception.printStackTrace();
 		}
 	}
-
+	
+	public void connectToGameChannelByName(String name) {
+		try {
+			gameChannel.exchangeDeclare(name, "fanout");
+			gameQueueName = gameChannel.queueDeclare().getQueue();
+			gameChannel.queueBind(gameQueueName, name, "");
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+	}
+	
+	public void disconnectFromGameChannel() {
+		try {
+			gameChannel.queueDelete(gameQueueName);
+			gameChannel.exchangeDelete(gameQueueName);
+			gameQueueName = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(Message message) {
+		try {
+			directChannel.basicPublish("", directQueueName, null, message.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Temporary
+	public String receiveStringFromDirect() {
+		try {
+			QueueingConsumer.Delivery delivery = directConsumer.nextDelivery();// <= tu nie dzia³a
+			return new String(delivery.getBody());
+		} catch (ShutdownSignalException e) {
+			e.printStackTrace();
+		} catch (ConsumerCancelledException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new String();
+	}
+	
+	public String receiveStringFromGame() {
+		try {
+			QueueingConsumer.Delivery delivery = gameConsumer.nextDelivery();// <= tu nie dzia³a
+			return new String(delivery.getBody());
+		} catch (ShutdownSignalException e) {
+			e.printStackTrace();
+		} catch (ConsumerCancelledException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new String();
+	}
 }
